bcce1dff0c2260a1fab63b86f984d750c818275e
Juste Raimbault <juste.raimbault@polytechnique.edu>
2016-05-02 16:33:53 +0200

    remrun

--- Models/Game/computeTrajs.R
+++ Models/Game/computeTrajs.R
@@ -4,14 +4,14 @@
 library(dplyr)
 library(ggplot2)
 
-setwd(paste0(Sys.getenv('CS_HOME'),'/MediationEcotox/Models/Game'))
+#setwd(paste0(Sys.getenv('CS_HOME'),'/MediationEcotox/Models/Game'))
 
 source('functions.R')
 
 datadir = '/mnt/volume1/juste/ComplexSystems/MediationEcotox/Results/Game/20160427_LHSGRID/split/'
 
 for(prelevementProba in c(0.6,0.8,1.0)){for(predatorCarrying in c(0.01,0.015,0.02)){for(preyReproduction in c(0.01,0.015,0.02)){
-  fileprefix=paste0(datadir,predatorCarrying,'-',format(prelevementProba,nsmall=1),'-',ppreyReproduction)
+  fileprefix=paste0(datadir,predatorCarrying,'-',format(prelevementProba,nsmall=1),'-',preyReproduction)
   res = getTrajs(list(prelevementProba=prelevementProba,predatorCarrying=predatorCarrying,preyReproduction=preyReproduction,filetype='.csv'),datadir,FALSE)
   trajx = res$trajx;trajy=res$trajy;trajs=res$trajs
   traj0 = trajs[,c(2,1)]
